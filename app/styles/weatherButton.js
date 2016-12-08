7c22eb037381436b620e941c8adc098411643749
RSamokhin <poshliemail@googlemail.com>
2016-12-08 12:40:15 +0200

    animate reactnative fix

--- app/styles/weatherButton.js
+++ app/styles/weatherButton.js
@@ -1,4 +1,3 @@
-
 module.exports = function (d) {
     return {
         container: {
--- app/views/WeatherPage.js
+++ app/views/WeatherPage.js
@@ -13,7 +13,6 @@ import {
     Text,
     TouchableOpacity,
     ScrollView,
-    Button,
     View
 } from 'react-native';
 
